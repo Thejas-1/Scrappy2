2b55f8ba71e4890e34521f4ae907983846ad5415
Thejas-1 <naiktheju@gmail.com>
2018-02-05 00:37:36 +0530

    Temporary scraper

--- scraper.py
+++ scraper.py
@@ -4,9 +4,13 @@ from bs4 import BeautifulSoup
 url = "https://www.researchgate.net/institution/PES_Institute_of_Technology/members"
 
 r = requests.get(url)
+#print r.content
 
-soup = BeautifulSoup(r.content,'lxml')
+soup = BeautifulSoup(r.content,"lxml")
+#unicode_str = r.decode("utf-8")
+#encoded_str = unicode_str.encode("ascii",'ignore')
+#soup = BeautifulSoup(encoded_str,"html.parser")
 
-links = soup.find_all("div")
+links = soup.find_all("li")
 
 print links
