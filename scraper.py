f5aa4f7b8036e597c9079cc69ab81175fa09236a
Thejas-1 <naiktheju@gmail.com>
2018-02-04 23:54:47 +0530

    Added scraper code

---
+++ scraper.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.researchgate.net/institution/PES_Institute_of_Technology/members"
+
+r = requests.get(url)
+
+soup = BeautifulSoup(r.content,'lxml')
+
+links = soup.find_all("a")
+
+print links
